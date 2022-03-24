01984e9b8b24380494b0d8a5ff16060e99581887
sandip <sandipkurmi0@gmail.com>
2022-03-24 11:28:05 +0530

    working with session and cookies

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000 //WE ARE SET SERVER PORT
 var mongoose = require('mongoose');
 const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+
 
 const url = "mongodb://localhost:27017/pizza"
 mongoose.connect(url, {useNewUrlParser: true});
@@ -33,18 +36,24 @@ conn.on('error', console.error.bind(console, 'connection error:'));
 //     console.log('connection failed...')
 // });
 
+//session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: conn,  
+    collection: 'sessions'
+})
 
-//assets 
 
 //session config :-> hear we create secret key and we put every secret key in env file for security resion
 app.use(session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
+    store: mongoStore,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24}  //only for 24 hours
 }))
 
-
+app.use(flash())
+//assets 
 app.use(express.static('public'))
 //set templet engin
 app.use(expressLayout)
